9d2d07c3b58c72e6bd183473df10e023ea3ef7e3
DESKTOP-VEM2IMA\sebas <sebastian.peredom@gmail.com>
2021-04-18 23:25:55 -0500

    swagger

--- handler.js
+++ handler.js
@@ -1,10 +1,34 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./src/index');
-require('./db/conexionBD');
+/*
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+*/
 
 require('./helper/utils_helper');
 require('./helper/constants');
+/*
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Cliente API',
+      version: '1.0.0',
+      description: 'Cliente API',
+      contact: {
+        name: 'Sebastian Peredo Murga',
+        url: 'https://www.linkedin.com/in/anthony-sebastian-peredo-murga-bb5938149/'
+      },
+    },
+    servers: ['http://localhost:3000/dev']
+  },
+  apis: ['./src/index.js']
+  // apis: ['handler.js']
+}
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+*/
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
--- src/cliente/c_cliente.js
+++ src/cliente/c_cliente.js
@@ -4,13 +4,14 @@ const m_cliente = require('./m_cliente');
 
 async function crearCliente(req, res) {
     try {
-        console.log('111')
         let { nombre,
               apellido,
               fecha_nacimiento
             } = req.body;
-        console.log('222')
-        global.__isNullOrEmpty([nombre, apellido, fecha_nacimiento], 'Faltan datos requeridos.');
+        
+        global.__isNullOrEmpty(nombre, 'Por favor, ingresa el nombre.');
+        global.__isNullOrEmpty(apellido, 'Por favor, ingresa el apellido.');
+        global.__isNullOrEmpty(fecha_nacimiento, 'Por favor, ingresa la fecha de nacimiento.');
 
         global.__maxLength(nombre, 55, 'El nombre no puede tener más de 55 letras.');
         global.__maxLength(apellido, 55, 'El apellido no puede tener más de 55 letras.');
@@ -19,14 +20,9 @@ async function crearCliente(req, res) {
         global.__maxDaysFromDate(fecha_nacimiento, 0, 'La fecha de nacimiento no puede ser futura.');
         global.__minYearsFromDate(fecha_nacimiento, 100, 'No podemos registrar clientes con más de 100 años.');
         
-        console.log('333')
-
         let result = await m_cliente.crearCliente(nombre, apellido, fecha_nacimiento);
         
-        console.log('4444')
-        
         return res.status(global.HTTP_200).send(result);
-
     } catch (error) {
         return global.responseError(error, res);
     }
